cc7c02ba7161fbb5565720cef839aee4cdd0c181
rkociniewski <112274712+rkociniewski@users.noreply.github.com>
2024-06-19 01:32:04 +0200

    change test to parametrized tests

--- src/test/kotlin/PalindromeTest.kt
+++ src/test/kotlin/PalindromeTest.kt
@@ -1,20 +1,38 @@
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertAll
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class PalindromeTest {
-    @Test
-    fun `should return true for palindromes`() = assertAll(
-        { assertTrue("Madam, in Eden, I'm Adam".isPalindrome()) },
-        { assertTrue("Flee to me, remote elf!".isPalindrome()) },
-        { assertTrue("Sex at noon taxes".isPalindrome()) },
-        { assertTrue("A Santa pets rats, as Pat taps a star step at NASA".isPalindrome()) }
+    @ParameterizedTest
+    @MethodSource("palindromeProvider")
+    fun `should return true for palindromes`(palindrome: String) {
+        assertTrue { palindrome.isPalindrome() }
+    }
+
+    @ParameterizedTest
+    @MethodSource("notPalindromeProvider")
+    fun `should return false if string is not palindromes`(palindrome: String) {
+        assertFalse { palindrome.isPalindrome() }
+    }
+
+    private fun palindromeProvider() = listOf(
+        Arguments.of("Madam, in Eden, I'm Adam"),
+        Arguments.of("Flee to me, remote elf!"),
+        Arguments.of("Sex at noon taxes"),
+        Arguments.of("A Santa pets rats, as Pat taps a star step at NASA"),
     )
 
-    @Test
-    fun `should return false if string is not palindromes`() = assertAll(
-        { assertFalse("xxxs".isPalindrome()) },
-        { assertFalse("this is not palindrome".isPalindrome()) },
+    private fun notPalindromeProvider() = listOf(
+        Arguments.of("Madam, Eden, I'm Adam"),
+        Arguments.of("Flee to e, remote elf!"),
+        Arguments.of("Sex at oon taxes"),
+        Arguments.of("A Santa ets rats, as Pat taps a star step at NASA"),
     )
 }
+
+
+
